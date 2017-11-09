c68580c67c8d931e1a792bfedc7e07d5997277e0
Gabriel Falcão <gabriel@nacaolivre.org>
2017-11-09 02:27:58 -0500

    oauth?

--- blogimporter/http.py
+++ blogimporter/http.py
@@ -5,6 +5,7 @@ import codecs
 from envelop import Environment
 from p4rr0t007.web import Application
 from plant import Node
+from flask_oauthlib.client import OAuth
 
 root_node = Node(__file__).dir
 config_node = root_node.cd('config.yml')
@@ -24,6 +25,9 @@ if config_node.exists is not True:
 
 config = Environment.from_file(config_node.path)
 
+
+client_id = config.get('blogger_outh_client_id')
+client_secret = config.get('blogger_outh_client_secret')
 secret_key = config.get('server_secret') or codecs.encode(os.urandom(32), 'hex')
 
 options = dict(
@@ -33,3 +37,22 @@ options = dict(
 )
 
 server = Application(root_node, **options)
+server.config['GOOGLE_ID'] = client_id
+server.config['GOOGLE_SECRET'] = client_secret
+
+
+oauth = OAuth(server)
+
+google = oauth.remote_app(
+    'google',
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    consumer_key=client_id,
+    consumer_secret=client_secret,
+)
--- blogimporter/views.py
+++ blogimporter/views.py
@@ -1,10 +1,32 @@
-from blogimporter.http import server
+# -*- coding: utf-8 -*-
+
+from flask import redirect, url_for, session
+from blogimporter.http import server, google
 from blogimporter.controllers import get_example_snippets
+
+
+@server.route('/oauth/callback')
+@google.authorized_handler
+def authorized(data, *args, **kw):
+    access_token = data['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
 
 
 @server.route('/')
 def index():
     return server.template_response('index.html')
+
+
+@server.route('/login')
+def login():
+    callback = url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
 
 
 @server.route('/snippets')
